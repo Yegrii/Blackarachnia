51191cf5feec1f5d0f8a54afd25b966216c66870
Yegor <viryutinyegor@gmail.com>
2023-03-25 17:08:06 +0200

    ChatGPT write this code and it's working

--- Blackarachnia/Proj_Aziz/engine.py
+++ Blackarachnia/Proj_Aziz/engine.py
@@ -1,9 +1,13 @@
+import os
 import logging
 
-import youtube_dl
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+from pytube import YouTube
 
 
-def download_video(video_link):
+def download_video(video_link: str) -> str:
     """Downloads the video from YouTube.
 
     Args:
@@ -13,30 +17,67 @@ def download_video(video_link):
         The path to the downloaded video.
     """
 
-    # Get the video ID.
-    video_id = youtube_dl.YoutubeDL({}).extract_video_id(video_link)
+    # Create a YouTube object with the video link.
+    youtube = YouTube(video_link)
 
-    # Try to download the video.
+    # Get the highest quality stream for the video.
+    stream = youtube.streams.get_highest_resolution()
+
+    # Download the video to the current working directory.
+    video_path = stream.download()
+
+    return video_path
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Handler for the /start command.
+
+    Args:
+        update: The Telegram update object.
+        context: The Telegram context object.
+
+    Returns:
+        None.
+    """
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Send me a YouTube link to download a video.")
+
+
+def download_video_handler(update: Update, context: CallbackContext) -> None:
+    """Handler for downloading a video from a YouTube link.
+
+    Args:
+        update: The Telegram update object.
+        context: The Telegram context object.
+
+    Returns:
+        None.
+    """
+
+    # Get the video link from the user's message.
+    video_link = update.message.text
+
+    # Download the video.
     try:
-        with youtube_dl.YoutubeDL({}) as ydl:
-            ydl.download([video_id])
-            return ydl.download_dir()
-    except:
-        return None
+        video_path = download_video(video_link)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Video downloaded to {video_path}")
+    except Exception as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Error downloading video: {e}")
 
 
-def main():
+def main() -> None:
     """Initialize the bot.
 
     Args:
-        None
+        None.
 
     Returns:
-        None
+        None.
     """
 
     # Get the token from the settings file.
-    token = '1111111111111111111'
+    with open('settings.py', 'r') as f:
+        token = f.read().strip().split('=')[1].strip().strip("'")
 
     # Create the bot.
     bot = Updater(token, use_context=True)
@@ -45,7 +86,7 @@ def main():
     bot.dispatcher.add_handler(CommandHandler('start', start))
 
     # Add the download video handler.
-    bot.dispatcher.add_handler(MessageHandler(Filters.text, download_video))
+    bot.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, download_video_handler))
 
     # Start the bot.
     bot.start_polling()
